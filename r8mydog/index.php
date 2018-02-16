da2fde1b52684d0851105d8671b5d90ef3af35ff
Spiffpitt <matthew6174@gmail.com>
2018-02-15 19:46:44 -0600

    added base site dir

---
+++ r8mydog/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Working on it...</title>
+	</head>
+	<body>
+		The site is still being built, please check back soon!
+	</body>
+</html>
