bd6e0d0ef9b1d755effd2accfb6d99ba70c2bcd4
Spiffpitt <matthew6174@gmail.com>
2018-02-16 13:40:13 -0600

    removed the img from database because the file path will be derived

--- database.sql
+++ database.sql
@@ -7,7 +7,6 @@ userid		integer(11)		not null,
 email		varchar(100)	not null,
 fname		varchar(25)		not null,
 lname		varchar(25)				,
-img			varchar(50)				,
 passhash	varchar(255)	not null,
 admin		boolean			not null,
 primary key (userid)
@@ -18,7 +17,6 @@ postid		integer(11)		not null,
 userid		integer(11)		not null,
 title		varchar(50)		not null,
 description	varchar(200)			,
-img			varchar(50)				,
 primary key (postid),
 foreign key (userid) references users(userid)
 );
@@ -29,7 +27,6 @@ userid		integer(11)		not null,
 postid		integer(11)		not null,
 title		varchar(50)		not null,
 description	varchar(200)			,
-img			varchar(50)				,
 primary key (ratingid),
 foreign key (postid) references posts(postid),
 foreign key (userid) references users(userid)
