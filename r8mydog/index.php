9b92a896159b7a678fcd6d2fbecdd53b39df14b0
Spiffpitt <matthew6174@gmail.com>
2018-02-20 14:05:34 -0600

    Changed line endings

--- r8mydog/index.php
+++ r8mydog/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>r8 my dog</title>
-		<?php include 'styles.php'; ?>
+		<link rel="stylesheet" href="site.css">
 	</head>
 	<body>
 		<a href="profile.php">View your profile.</a><br>
