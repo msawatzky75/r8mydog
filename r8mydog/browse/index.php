59e3fdb9ddd540905ebf22c566490930dde70cfc
Spiffpitt <matthew6174@gmail.com>
2018-02-21 14:51:39 -0600

    Added new page templates

---
+++ r8mydog/browse/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>r8mydog - Search</title>
+	<?php require '../snippet/bootstrap.html'; ?>
+</head>
+<body>
+	<?php require '../snippet/header.php'; ?>
+
+</body>
+</html>
