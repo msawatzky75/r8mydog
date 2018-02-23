34745f60875226ec9a7d34abc61116e0422ba1f0
Spiffpitt <matthew6174@gmail.com>
2018-02-23 16:42:04 -0600

    Updated Content

--- r8mydog/profile/index.php
+++ r8mydog/profile/index.php
@@ -1,15 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>r8mydog - Search</title>
+	<title>r8mydog - Profile - Details</title>
 	<?php require '../snippet/bootstrap.html'; ?>
 </head>
 <body>
 	<?php require '../snippet/header.php'; ?>
-	<section class="container">
-		<h1><?=$_SESSION['fname'].' '.$_SESSION['lname']?>'s profile</h1>
-		<p>Email: <?=$_SESSION['email']?></p>
-		<p>Admin: <?=$_SESSION['admin'] ? "Yes" : "No"?></p>
+	<section class="container"> <!-- maybe we should do this in js-->
+		<?php if (isset($_SESSION['userid'])) : ?>
+			<?php if (isset($_GET['details']) || isset($_GET['edit'])) : ?>
+				<h1 class="mt-1"><?=$_SESSION['fname'].' '.$_SESSION['lname']?>'s profile <span class='badge badge-info float-right mt-2 d-none d-lg-block'><?=$_SESSION['admin'] ? "Administrator" : "Standard"?></span></h1>
+				<hr>
+				<form>
+					<!-- fname -->
+					<div class="form-group row">
+						<label for="email" class="col-md-2 col-form-label">First Name</label>
+						<div class="col-md-10">
+							<input type="text" <?= isset($_GET['details']) ? 'readonly class="form-control-plaintext"' : 'class="form-control"' ?> id="email" value="<?=$_SESSION['fname']?>">
+						</div>
+					</div>
+
+					<!-- lname -->
+					<div class="form-group row">
+						<label for="email" class="col-md-2 col-form-label">Last Name</label>
+						<div class="col-md-10">
+							<input type="text" <?= isset($_GET['details']) ? 'readonly class="form-control-plaintext"' : 'class="form-control"' ?> id="email" value="<?=$_SESSION['lname']?>">
+						</div>
+					</div>
+
+					<!-- email -->
+					<div class="form-group row">
+						<label for="email" class="col-md-2 col-form-label">Email</label>
+						<div class="col-md-10">
+							<input type="text" <?= isset($_GET['details']) ? 'readonly class="form-control-plaintext"' : 'class="form-control"' ?> id="email" value="<?=$_SESSION['email']?>">
+						</div>
+					</div>
+
+					<!-- buttons -->
+					<div class="form-group row justify-content-end">
+
+						<?php if (isset($_GET['details'])) : ?>
+							<div class="col-md-2 col-sm-4">
+								<a class="btn btn-secondary col-sm-12" href="/profile?edit" name="edit">Edit</a>
+							</div>
+
+						<?php elseif (isset($_GET['edit'])): ?>
+							<div class="col-md-2 col-sm-4">
+								<a class="btn btn-secondary col-sm-12" href="/profile?details" name="delete">Back</a>
+							</div>
+							<div class="col-md-2 col-sm-4 mt-2 mt-sm-0">
+								<input class="btn btn-primary col-sm-12" action="/snippets/insertAndValidate?updateprofile" method="post" name="save" value="Save">
+							</div>
+							<div class="col-md-2 col-sm-4 mt-2 mt-sm-0">
+								<a class="btn btn-danger col-sm-12" href="/profile?delete" name="delete">Delete <span class="d-none d-sm-inline d-md-none d-xl-inline">Account</span></a>
+							</div>
+
+						<?php endif; ?>
+					</div>
+				</form>
+				<!-- end details veiw -->
+			<?php else : ?>
+
+			<?php endif;  //if $_GET ?>
+		<?php endif;  //if session is set ?>
 	</section>
 </body>
 </html>
