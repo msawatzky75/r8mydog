2f4d8079931dda477c4dacb561570e1b5fd8b920
Spiffpitt <matthew6174@gmail.com>
2018-03-19 13:59:18 -0500

    Updated repo name

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -1,7 +1,7 @@
 #delete old files
 rm -rf /home/spiffpitt/public_html/
 #download the new ones
-wget -P /home/spiffpitt/public_html https://github.com/spiffpitt/WEBD-2006-Project/archive/master.zip
+wget -P /home/spiffpitt/public_html https://github.com/spiffpitt/r8mydog/archive/master.zip
 #unzip the new ones
 unzip /home/spiffpitt/public_html/master.zip -d /home/spiffpitt/public_html/
 #remove the new zip folder, it isnt needed anymore
@@ -10,13 +10,13 @@ rm /home/spiffpitt/public_html/master.zip
 #allow the movement of hidden files
 shopt -s dotglob
 #move all files from the r8mydog folder to the root
-mv /home/spiffpitt/public_html/WEBD-2006-Project-master/r8mydog/* /home/spiffpitt/public_html/
+mv /home/spiffpitt/public_html/r8mydog-master/r8mydog/* /home/spiffpitt/public_html/
 
 #update the auto deploy
 chmod /home/spiffpitt/autoDeploy.sh 775
-mv /home/spiffpitt/public_html/WEBD-2006-Project-master/autoDeploy.sh /home/spiffpitt/
+mv /home/spiffpitt/public_html/r8mydog-master/autoDeploy.sh /home/spiffpitt/
 
 #remove uneeded files
-rm -rf /home/spiffpitt/public_html/WEBD-2006-Project-master
+rm -rf /home/spiffpitt/public_html/r8mydog-master
 #list the files for verification
 #ls /home/spiffpitt/public_html/ -AGl
