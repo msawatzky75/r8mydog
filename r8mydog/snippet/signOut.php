741e0db88a975cc7a0abe5b1de8220bf25675c1e
Spiffpitt <matthew6174@gmail.com>
2018-03-22 21:34:59 -0500

    Removed cookies

--- r8mydog/snippet/signOut.php
+++ r8mydog/snippet/signOut.php
@@ -1,7 +1,20 @@
 <?php
 session_start();
+unset($_SESSION);
 session_destroy();
 //remove cookies
-
+// echo '<pre>';
+// print_r($_COOKIE);
+// echo '</pre>';
+// unset($_COOKIE['userid']);
+// unset($_COOKIE['fname']);
+// unset($_COOKIE['lname']);
+// unset($_COOKIE['email']);
+// unset($_COOKIE['admin']);
+// unset($_COOKIE);
+// echo '<pre>';
+// print_r($_COOKIE);
+// echo '</pre>';
+//redirect
 header("location:/");
 ?>
