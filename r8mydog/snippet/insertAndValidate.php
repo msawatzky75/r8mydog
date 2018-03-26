e22e579ab74fa31910527bdf4f5012c7e428acaa
Spiffpitt <matthew6174@gmail.com>
2018-03-26 12:10:25 -0500

    CAPTCHA verification

--- r8mydog/snippet/insertAndValidate.php
+++ r8mydog/snippet/insertAndValidate.php
@@ -66,7 +66,18 @@ if ($_POST)
 	}
 	else if ($_POST['type'] == 'post')
 	{
-		session_start();
+		include_once '../lib/securimage/securimage.php';
+		$securimage = new Securimage();
+		if(!isset($_SESSION))
+			session_start();
+		if ($securimage->check($_POST['captcha_code']) == false) {
+			// the code was incorrect
+			// you should handle the error so that the form processor doesn't continue
+			// or you can use the following code if there is no validation or you do not know how
+			echo "The security code entered was incorrect.<br /><br />";
+			echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+			die();
+		}
 		//img upload
 		//title, description
 		$title = filter_var(trim($_POST['title']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
