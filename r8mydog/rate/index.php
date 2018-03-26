e7dcf5c2eb2a2071192279452f3e25ffa44dbce2
Spiffpitt <matthew6174@gmail.com>
2018-03-25 23:07:30 -0500

    Added the rate page outline

---
+++ r8mydog/rate/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate - r8mydog</title>
+	<?php require '../snippet/bootstrap.html'; ?>
+</head>
+<body>
+	<?php require '../snippet/header.php'; ?>
+	<section class="container">
+
+	</section>
+</body>
+</html>
