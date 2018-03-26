d282e77c0ebaf25694f2561fa29bfb35f80d7517
Spiffpitt <matthew6174@gmail.com>
2018-03-26 12:12:02 -0500

    Ordered users

--- r8mydog/snippet/users.php
+++ r8mydog/snippet/users.php
@@ -1,7 +1,7 @@
 <?php
 //writes pure json for the ajax request from the posts page for the list of users
 require '../snippet/connect.php';
-$query = "SELECT userid, fname, lname, CONCAT(CONCAT(fname, ' '), lname) AS fullname FROM users;";
+$query = "SELECT userid, fname, lname, CONCAT(CONCAT(fname, ' '), lname) AS fullname FROM users ORDER BY fname, lname;";
 $statement = $db->prepare($query);
 $statement->execute();
 echo json_encode($statement->fetchAll());
