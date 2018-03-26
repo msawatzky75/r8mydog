3ae52de0bd3afa8eb010b57da1be4d3be1a9688a
Spiffpitt <matthew6174@gmail.com>
2018-03-25 23:29:45 -0500

    Removed broken links

--- r8mydog/about/index.php
+++ r8mydog/about/index.php
@@ -8,8 +8,8 @@
 	<?php require '../snippet/header.php'; ?>
 	<section class="container">
 		<h1>About r8mydog</h1>
-		<p>I, <a href="/profiles?u=spiffpitt"><strong>Matthew Paul Sawatzky</strong></a> am currently working on devolping r8mydog as a Content Managment System website. r8mydog is a hot dog rating site where users post photos with titles and captions for others to rate.</p>
-		<p>I credit the idea to my friend <a href="/profiles?u=pxlnght"><strong>Brian</strong></a> who prompted a site to rate hot dogs when I had asked. </p>
+		<p>I, <strong>Matthew Paul Sawatzky</strong> am currently working on devolping r8mydog as a Content Managment System website. r8mydog is a hot dog rating site where users post photos with titles and captions for others to rate.</p>
+		<p>I credit the idea to my friend <strong>Brian</strong> who prompted a site to rate hot dogs when I had asked. </p>
 		<p>While I am still devolping the website, i am still open to ideas feel free to let me know at <a href="mailto:reluctantbee@gmail.com">reluctantbee@gmail.com</a>.</p>
 	</section>
 </body>
