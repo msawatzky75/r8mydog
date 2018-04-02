3c636cbd3115d8eb77d2ca92d190aad16ad5ed92
Spiffpitt <matthew6174@gmail.com>
2018-04-01 23:03:18 -0500

    Added welcome page

---
+++ r8mydog/welcome/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home - r8mydog</title>
+		<?php require '../snippet/bootstrap.html'; ?>
+	</head>
+	<body>
+		<?php require '../snippet/header.php'; ?>
+		<?php //if ($loggedin) { header("location:/post"); } ?>
+		<section class="container">
+			<h1 class="text-center mt-3">r8mydog</h1>
+			<p class="text-center">Welcome to the worlds best hot dog rating website!</p>
+			<h4 class="text-center">Get started with just a few steps!</h4>
+			<section class="d-flex">
+				<ul class="mx-auto justify-content-center text-center list-unstyled">
+					<li><h5><a href="/register">Make an account</a></h5></li>
+					<li><h5><a href="/post/new">Create a post</a></h5></li>
+					<li><h5><a href="/post">Rate other posts</a></h5></li>
+				</ul>
+			</section>
+		</section>
+	</body>
+</html>
